f1196a5eeb46afb2c48187db753aa780836b753e
JGardner01 <jamesjmg@live.co.uk>
2024-11-15 22:43:26 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module scc402.userstudy {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens scc402.userstudy to javafx.fxml;
+    exports scc402.userstudy;
+}
