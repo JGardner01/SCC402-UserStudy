d984432b374e1a9d2873a4a0e63d2c4a2cabae0e
JGardner01 <jamesjmg@live.co.uk>
2024-11-15 23:01:01 +0000

    Refactored starting default files. Created start scene for intro to part 1.

---
+++ src/main/java/scc402/userstudy/MainController.java
@@ -0,0 +1,11 @@
+package scc402.userstudy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    protected void onStartButtonClick() {
+        System.out.println("Starting User Study");
+    }
+}
